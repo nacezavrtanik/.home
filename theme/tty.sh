56227f155fc4cd026f4be735b62a41c0a6169473
Nace Zavrtanik <zavrtaniknace@gmail.com>
2026-06-25 23:32:34 +0200

    theme: tty theme added

---
+++ theme/tty.sh
@@ -0,0 +1,19 @@
+
+echo -e '\e]P0000000'
+echo -e '\e]P1db662d'
+echo -e '\e]P224604a'
+echo -e '\e]P3feaf3c'
+echo -e '\e]P4255a62'
+echo -e '\e]P5822847'
+echo -e '\e]P61a7776'
+echo -e '\e]P7e5c49e'
+echo -e '\e]P8202020'
+echo -e '\e]P9fb864d'
+echo -e '\e]Pa44806a'
+echo -e '\e]Pbffbb65'
+echo -e '\e]Pc457a82'
+echo -e '\e]Pda24867'
+echo -e '\e]Pe3a9796'
+echo -e '\e]Pfffdeba'
+
+clear
